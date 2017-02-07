a010d2b4a6cf4b931e52962fb5f949733cfdf871
Mark Sansait <marks@ocisi.com>
2017-02-07 14:14:38 -0600

    Added ComicBook Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,12 +7,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 } 
