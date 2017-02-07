2944f99d3e33f9c7abd04968692eacb67ce5d723
Mark Sansait <marks@ocisi.com>
2017-02-07 13:43:21 -0600

    added Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
