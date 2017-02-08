bf4b9871c5d98924d2970089c655c3977b2667b7
Mark Sansait <marks@ocisi.com>
2017-02-08 16:37:34 -0600

    Added comic books list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -57,6 +57,12 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
          };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
